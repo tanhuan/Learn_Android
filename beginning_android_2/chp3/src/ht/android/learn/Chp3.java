595e566978733af2a57660fadeb70a7c7fa0bada
Huan <tanhuan@gmail.com>
2011-02-03 17:35:27 -0500

    original commit

---
+++ beginning_android_2/chp3/src/ht/android/learn/Chp3.java
@@ -0,0 +1,36 @@
+package ht.android.learn;
+
+import java.util.Date;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Chp3 extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+	Button btn;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        btn = (Button)findViewById(R.id.button);
+        
+        btn.setOnClickListener(this);
+        updateTime();
+
+    }
+
+	private void updateTime() {
+		// TODO Auto-generated method stub
+		btn.setText("Chp3:\n" + new Date().toString());
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		updateTime();
+	}
+}
